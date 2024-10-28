f62ee4160c40fd52d8f6225ae7ff70360c581417
Stuart Lang <stuart.b.lang@gmail.com>
2024-10-28 16:55:57 +0000

    Add sqs tag tests

--- tests/LocalSqsSnsMessaging.Tests.AppHost/Program.cs
+++ tests/LocalSqsSnsMessaging.Tests.AppHost/Program.cs
@@ -1,7 +1,16 @@
 ﻿var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddContainer("localstack", "localstack/localstack", "stable")
+var localstack = builder.AddContainer("localstack", "localstack/localstack", "stable")
     .WithHttpEndpoint(targetPort: 4566)
     .WithEnvironment("SERVICES", "sqs,sns");
+
+var isRunningInCi = Environment.GetEnvironmentVariable("CI") == "true";
+
+if (!isRunningInCi)
+{
+    localstack
+    .WithContainerName("localsqssnsmessaging-localstack")
+    .WithLifetime(ContainerLifetime.Persistent);
+}
     
 builder.Build().Run();
---
+++ tests/LocalSqsSnsMessaging.Tests/LocalAwsMessaging/SqsQueueTagsLocalAwsMessagingTests.cs
@@ -0,0 +1,25 @@
+using Microsoft.Extensions.Time.Testing;
+
+namespace LocalSqsSnsMessaging.Tests.LocalAwsMessaging;
+
+// ReSharper disable once UnusedType.Global
+public class SqsQueueTagsLocalAwsMessagingTests : SqsQueueTagsTests
+{
+    private readonly FakeTimeProvider _timeProvider;
+    
+    public SqsQueueTagsLocalAwsMessagingTests()
+    {
+        _timeProvider = new FakeTimeProvider();
+        var bus = new InMemoryAwsBus
+        {
+            TimeProvider = _timeProvider
+        };
+        Sqs = bus.CreateSqsClient();
+    }
+
+    protected override Task AdvanceTime(TimeSpan timeSpan)
+    {
+        _timeProvider.Advance(timeSpan);
+        return Task.CompletedTask;
+    }
+}
